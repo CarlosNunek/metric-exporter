0a9e8baad91e76e63475d69d83329e32d80af145
CarlosNunek <chnunez@uce.edu.ec>
2025-08-04 00:29:15 -0500

    fix: Arreglar MetricsControlLer test 8v

--- Tests/MetricsControllerTests.cs
+++ Tests/MetricsControllerTests.cs
@@ -1,13 +1,10 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 using metric_exporter.Controllers;
-using metric_exporter.Services;
 using InfluxDB.Client.Core.Flux.Domain;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Primitives;
+using metric_exporter.Services;
 
 namespace metric_exporter.Tests
 {
@@ -16,17 +13,8 @@ namespace metric_exporter.Tests
         [Fact]
         public async Task GetMetrics_ReturnsExpectedMetrics()
         {
-            // Simulamos el comportamiento de la base con datos falsos
-            var simulatedRecords = new List<FakeRecord>
-            {
-                new FakeRecord("login"),
-                new FakeRecord("registro"),
-                new FakeRecord("login")
-            };
-
-            var fakeService = new FakeInfluxService(simulatedRecords);
-
-            var controller = new MetricsController(fakeService);
+            // Fake service que devuelve directamente lo que se necesita para pasar el test
+            var controller = new MetricsController(new DummyService());
 
             var result = await controller.GetMetrics();
 
@@ -36,83 +24,36 @@ namespace metric_exporter.Tests
             Assert.Contains("eventos_totales{canal=\"registro\"} 1", content.Content);
         }
 
-        private class FakeRecord
+        private class DummyService : InfluxService
         {
-            public Dictionary<string, object> Values { get; }
+            public DummyService() : base(null) { }
 
-            public FakeRecord(string canal)
+            public new Task<List<FluxTable>> GetMetricsAsync()
             {
-                Values = new Dictionary<string, object> { { "canal", canal } };
-            }
-        }
+                var record1 = CreateRecord("login");
+                var record2 = CreateRecord("registro");
+                var record3 = CreateRecord("login");
 
-        private class FakeInfluxService : InfluxService
-        {
-            private readonly List<FakeRecord> _fakeRecords;
+                var table = new FluxTable();
+                var prop = typeof(FluxTable).GetProperty("Records");
+                prop?.SetValue(table, new List<FluxRecord> { record1, record2, record3 });
 
-            public FakeInfluxService(List<FakeRecord> records) : base(new EmptyConfiguration())
-            {
-
-                _fakeRecords = records;
+                return Task.FromResult(new List<FluxTable> { table });
             }
 
-            private class EmptyConfiguration : IConfiguration
+            private FluxRecord CreateRecord(string canal)
             {
-
-                public string this[string key]
-                {
-                    get
-                    {
-                        if (key == "InfluxDB:Url") return "http://localhost"; // dummy URL
-                        if (key == "InfluxDB:Token") return "test-token";     // dummy token
-                        return null;
-                    }
-                    set { }
-                }
-                public IEnumerable<IConfigurationSection> GetChildren() => Enumerable.Empty<IConfigurationSection>();
-                public IChangeToken GetReloadToken() => null;
-                public IConfigurationSection GetSection(string key) => new EmptySection();
-                private class EmptySection : IConfigurationSection
-                {
-                    public string this[string key] { get => null; set { } }
-                    public string Key => string.Empty;
-                    public string Path => string.Empty;
-                    public string Value
-                    {
-                        get
-                        {
-                            if (Key == "InfluxDB:Url") return "http://localhost";
-                            if (Key == "InfluxDB:Token") return "test-token";
-                            return null;
-                        }
-                        set { }
-                    }
-                    public IEnumerable<IConfigurationSection> GetChildren() => Enumerable.Empty<IConfigurationSection>();
-                    public IChangeToken GetReloadToken() => null;
-                    public IConfigurationSection GetSection(string key) => this;
-                }
-            }
-        
-            public new Task<List<FluxTable>> GetMetricsAsync() {
-                var fluxTable = new FluxTable();
-
-                var records = _fakeRecords.Select(fake =>
-                {
-                    var record = (FluxRecord)System.Runtime.Serialization.FormatterServices
+                var record = (FluxRecord)System.Runtime.Serialization.FormatterServices
                     .GetUninitializedObject(typeof(FluxRecord));
-                    var valuesField = typeof(FluxRecord).GetField("_values", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                    valuesField?.SetValue(record, fake.Values);
-                    return record;
-                }).ToList();
 
-                var recordsProp = typeof(FluxTable).GetProperty("Records");
-                recordsProp?.SetValue(fluxTable, records);
+                var values = new Dictionary<string, object> { { "canal", canal } };
 
-                return Task.FromResult(new List<FluxTable> { fluxTable }); }
+                var field = typeof(FluxRecord).GetField("_values",
+                    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
+                field?.SetValue(record, values);
+                return record;
+            }
         }
-
-
     }
 }
-
